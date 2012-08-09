f255f50d4960457c9567624ff27ed980cd4e3f73
William David Parras <william.parras.mendez@gmail.com>
2012-08-09 08:50:16 -0600

    Separando en modulos algunas funciones

--- mp3.py
+++ mp3.py
@@ -4,51 +4,7 @@
 # evitar ejecucion de versiones anteriores
 import pygtk,gtk
 pygtk.require("2.0")
-import os,glob,tree
-
-
-# funcion para creacion de botones con etiqueta e imagen
-def buttonBox(padre,icono,label_text):
-
-	# Crear main_frame para icon y label
-	box = gtk.HBox(True, 0)
-	# Creamos la imagen
-	imagen = gtk.Image()
-	imagen.set_from_file(icono)
-	# Creamos la etiqueta
-	label = gtk.Label(label_text)
-	# Empaquetamos icon y label en la main_frame
-	box.pack_start(imagen, False, False, 3)
-	box.pack_start(label, False, False, 3)
-	image.show()
-	label.show()
-	return box;
-
-# funcion para creacion de botones con imagen sin etiqueta
-def buttonBox2(padre,icono):
-
-	# Crear main_frame para icon
-	box = gtk.HBox(True, 0)
-	box.set_border_width(2)
-	# Creamos la imagen
-	image = gtk.Image()
-	image.set_from_file(icono)
-	# Empaquetamos icon en la main_frame
-	box.pack_start(image, False, False, 3)
-	image.show()
-	return box;
-
-# funcion para poner la escala a valores iniciales
-def scale_set_default_values(scale):
-
-	# politica de actualizacion 
-	scale.set_update_policy(gtk.UPDATE_CONTINUOUS)
-	# digitos a mostrar en la escala
-	scale.set_digits(1)
-	# pocision pre definida
-	scale.set_value_pos(gtk.POS_TOP)
-	# dibujado
-	scale.set_draw_value(True)
+import os,glob,tree,ext
 
 # clase principal
 class ventana:
@@ -92,6 +48,7 @@ class ventana:
 		gtk.main()
 
 	def __init__(self):
+
 		# Ventana gestionada por el manejador de ventana del sistema
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect("delete_event",self.delete)
@@ -99,7 +56,7 @@ class ventana:
 		self.window.set_title("Reproductor Mp3")
 		self.window.set_border_width(0)
                 
-		# Creacion de las tablas y main_frames
+		# Creacion de las tablas y frames
 		self.btn_nav = gtk.Table(9,1,True)
 		self.controles = gtk.Table(3,1,True)
 		self.vol_box_icon = gtk.Table(1,1,False)
@@ -112,76 +69,96 @@ class ventana:
 		self.campo_busq = gtk.Entry(0)
 		self.campo_busq.set_text("Busqueda...")
 
-		# menu
+		# Menu
+		# Creacion de los submenus
 		self.submenu_1 = gtk.Menu()
 		self.submenu_2 = gtk.Menu()
 		self.submenu_3 = gtk.Menu()
+		# Creacion de los Items de cada Submenu
+		# Para submenu_1
   		self.item1_1 = gtk.MenuItem("Abrir")
   		self.item1_2 = gtk.MenuItem("Actualizar")
   		self.item1_3 = gtk.MenuItem("Importar")
+		# Para submenu_2
 		self.item2_1 = gtk.MenuItem("Preferencias")
+		# Para submenu_3
 		self.item3_1 = gtk.MenuItem("Help F2")
-		self.item1_1.show()
-  		self.item1_2.show()
-  		self.item1_3.show()
-		self.item2_1.show()
-		self.item3_1.show()
+		# Mostrando Items
+		ext.show(self.item1_1,self.item1_2,self.item1_3,self.item2_1,self.item3_1)
+		# Agregando items a respectivos submenus
  		self.submenu_1.append(self.item1_1)
   		self.submenu_1.append(self.item1_2)
   		self.submenu_1.append(self.item1_3)
 		self.submenu_2.append(self.item2_1)
 		self.submenu_3.append(self.item3_1)
+		# Creando Items del menu principal
+		# Corresponde submenu1
 		self.archivo = gtk.MenuItem("Archivo")
+		# Corresponde submenu2
 		self.editar = gtk.MenuItem("Editar")
+		# Corresponde submenu3
 		self.ayuda = gtk.MenuItem("Ayuda")
+		# Agregando submenus a menu principal
 		self.archivo.set_submenu(self.submenu_1)
 		self.editar.set_submenu(self.submenu_2)
 		self.ayuda.set_submenu(self.submenu_3)
-		self.archivo.show()
-		self.editar.show()
-		self.ayuda.show()
+		# Mostrando submenus
+		ext.show(self.archivo,self.editar,self.ayuda)
+		# Eventos
 		self.item1_3.connect_object("activate", self.importar,None)
+		# Creando Barra de Menu
 		self.menu_bar = gtk.MenuBar()
-		self.menu_bar.show()	
+		self.menu_bar.show()
+		# Agregando a Barra de Menu	
 		self.menu_bar.append(self.archivo)
 		self.menu_bar.append(self.editar)
 		self.menu_bar.append(self.ayuda)
 
-                #Seccion con barras de desplazamiento
+                # Seccion con barras de desplazamiento
 		self.vent_list = tree.create_tree()
 		tree.update_tree(self.directorio,self.vent_list)
 		
-
 		# Control de volumen 
+		# Creacion del ajuste
 		self.ajuste = gtk.Adjustment(0.0, 0.0, 101.0, 0.1, 1.0, 1.0)
+		# Creacion del control con el ajuste previamente creado
 		self.control = gtk.HScale(self.ajuste)
-		scale_set_default_values(self.control)
+		# Poniendo valores y politicas por defecto
+		ext.scale_set_default_values(self.control)
+		# Icono del control de volumen
                 self.vIcon = gtk.Image()
                 self.vIcon.set_from_file("icons/volumen.png") 
                 
 		# Botones para el reproductor
-		self.plB = buttonBox2(self.window,"icons/play.png")
-		self.stB = buttonBox2(self.window,"icons/stop.png")
-		self.neB = buttonBox2(self.window,"icons/next.png")
-		self.prB = buttonBox2(self.window,"icons/prev.png")
-		self.paB = buttonBox2(self.window,"icons/pause.png")
+		# Creacion de cajas con iconos
+		self.plB = ext.buttonBox2(self.window,"icons/play.png")
+		self.stB = ext.buttonBox2(self.window,"icons/stop.png")
+		self.neB = ext.buttonBox2(self.window,"icons/next.png")
+		self.prB = ext.buttonBox2(self.window,"icons/prev.png")
+		self.paB = ext.buttonBox2(self.window,"icons/pause.png")
+		# Creacion de los botones
 		self.play = gtk.Button()
 		self.stop = gtk.Button()
 		self.next = gtk.Button()
 		self.prev = gtk.Button()
 		self.pause = gtk.Button()
+		# Agregando iconos a Botones
 		self.play.add(self.plB)
 		self.stop.add(self.stB)
 		self.next.add(self.neB)
 		self.prev.add(self.prB)
 		self.pause.add(self.paB)
-		# Pocisionando en tabla
+
+		# Pocisionando en tablas
+		# Tablas para botones
 		self.btn_nav.attach(self.play,0,1,0,1,gtk.FILL,gtk.FILL,0,0)
 		self.btn_nav.attach(self.stop,1,2,0,1,gtk.FILL,gtk.FILL,0,0)
 		self.btn_nav.attach(self.next,2,3,0,1,gtk.FILL,gtk.FILL,0,0)
 		self.btn_nav.attach(self.prev,3,4,0,1,gtk.FILL,gtk.FILL,0,0)
 		self.btn_nav.attach(self.pause,4,5,0,1,gtk.FILL,gtk.FILL,0,0)
+		# Tabla para icono de volumen
 		self.vol_box_icon.attach(self.vIcon,0,1,0,1,gtk.FILL,gtk.FILL,10,10)
+		# Tabla para control de volumen y caja de busqueda
 		self.controles.attach(self.control,1,2,0,1,gtk.FILL,gtk.FILL,0,0)
 		self.controles.attach(self.campo_busq,0,1,0,1,gtk.FILL,gtk.FILL,10,10)
 
@@ -193,28 +170,12 @@ class ventana:
 		self.div_2.pack_start(self.controles,False,False,0)
 		self.div_2.pack_start(self.vol_box_icon,False,False,0)
 		self.window.add(self.main_frame)
-
-		# Mostrando los respectivos elementos
-		self.vent_list.show()
-		self.vIcon.show()
-		self.campo_busq.show()
-		self.plB.show()
-		self.stB.show()
-		self.neB.show()
-		self.prB.show()
-		self.paB.show()
-		self.play.show()
-		self.stop.show()
-		self.next.show()
-		self.prev.show()
-		self.pause.show()
-		self.control.show()
-		self.btn_nav.show()
-		self.controles.show()
-		self.vol_box_icon.show()
-		self.main_frame.show()
-		self.div_2.show()
-		self.window.show()
+		
+		#Mostrando elementos
+		ext.show(self.vent_list,self.vIcon,self.campo_busq,self.plB,self.stB,self.neB)
+		ext.show(self.prB,self.paB,self.play,self.stop,self.next,self.prev)
+		ext.show(self.pause,self.control,self.btn_nav,self.controles)
+		ext.show(self.vol_box_icon,self.main_frame,self.div_2,self.window)
 
 # programa principal
 if __name__=="__main__":
