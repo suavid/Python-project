f72f905b1d2a5e2a2b5e5471609e3dd3261fe1a9
William David Parras <william.parras.mendez@gmail.com>
2012-08-14 21:00:33 -0600

    Comentando el codigo para su comprension

--- glmplayer.py
+++ glmplayer.py
@@ -3,6 +3,7 @@
 # gtk: permite integracion con el entorno
 # path: modulo propio con rutas y mensajes personalizados
 # os: permite comunicar con el os
+# xml.dom.minidom para manejo de documentos xml
 import gtk,path,os
 from xml.dom.minidom import Document
 from xml.dom import minidom
@@ -11,6 +12,7 @@ import gst
 filepattern = (
          ("MP3","*.mp3"),
         ) 
+# funcion para revertir una cadena de caracteres
 def reverse(list):
 	if len(list)==1:
 		return list
@@ -32,12 +34,20 @@ class main:
 		self.rep = 0 
 	 # Obtiene el objeto gtkliststore de glade y lo conecta	
 		self.medialist = builder.get_object("media")
+	 # Obtiene el manejador de selecciones
 		self.sel = builder.get_object("selec")
+	 # Estos objetos que siguen aun no se han utilizado en el codigo del programa
+	 # Obtiene el gtk.TreeWiev
 		self.tree = builder.get_object("arbol_pistas")
+	 # Obtiene el objeto Archivos que contiene el texto
 		self.ar = builder.get_object("Archivos")
+	 # Obtiene el gtk.Text hijo de Archivos
 		self.te = builder.get_object("Textos")
+	# Lee las pistas del documento XML	
 		try:
+			# Conecta al documento
 			dom = minidom.parse("playlist/track.xml")
+			# Agrega las pistas al medialist
 			for i in range(0,len(dom.getElementsByTagName("track"))):
 				self.medialist.append([dom.getElementsByTagName("track")[i].firstChild.data,dom.getElementsByTagName("ruta")[i].firstChild.data])
 		except:
@@ -55,15 +65,21 @@ class main:
 		"on_salir_activate":self.destroy
 		}
 		
+		# Conecta 
+		builder.connect_signals(dict)
+		
+		# Generacion del elemento controlador de la reproducccion
 		self.player = gst.element_factory_make("playbin2", "player")
+		# Obtiene el Bus
 		bus = self.player.get_bus()
+		# Agrega un vigilador de señales
 		bus.add_signal_watch()
+		# Habilita la emision de senales, esto ayuda con el control de Errores
 		bus.enable_sync_message_emission()
+		# Conecta las respectivas funciones de muestra de mensajes 
 		bus.connect("message", self.on_message)
 		bus.connect("sync-message::element", self.on_sync_message)
 		
-	  # Conecta 
-		builder.connect_signals(dict)
 	# Definicion de la funcion para agregar archivos de sonido
 	def abrir_archivos(self,widget):
 		# Tipos de archivos agregables
@@ -82,14 +98,16 @@ class main:
 		self.agregar_ventana.remove_filter(self.filtro)
 		# Termina ejecucion y se esconde la ventana
 		self.agregar_ventana.hide()
-		# Se procesa la respuesta de la ventana, -5 abrir -6 cancelar
+		# Abre en modo escritura el documento de pistas para sobreescribir
+		# Esto debe arreglarse
 		xmldocument = open("playlist/track.xml","w")
 		# Crea el documento minidom 
 		doc = Document()
 		# Crea el elemento base <wml> 
 		wml = doc.createElement("wml")
-		#lo agrega al documento
+		# Lo agrega al documento
 		doc.appendChild(wml)
+		# Se procesa la respuesta de la ventana, -5 abrir -6 cancelar
 		if respt == -5:        
 			fileselected = self.agregar_ventana.get_filenames()
 			for files in fileselected:
@@ -106,18 +124,27 @@ class main:
 				maincard.appendChild(dr)
 				path = doc.createTextNode(dirs)
 				dr.appendChild(path)
+		# guarda en el documento
 		xmldocument.write(doc.toprettyxml(indent="  "))
-		xmldocument.close()	
-		
+		# cierra el documento
+		xmldocument.close()
+			
+	# Funcion que emite los mensajes, asignada anteriomente 
 	def on_message(self, bus, message):
-		t = message.type
-		if t == gst.MESSAGE_EOS:
+		# Los parametros son pasados por el controlador del Bus
+		t = message.type 
+		# Obtiene el mensaje y lo procesa
+		if t == gst.MESSAGE_EOS: 
+			# Detiene la reproduccion
 			self.player.set_state(gst.STATE_NULL)
 		elif t == gst.MESSAGE_ERROR:
+			# Por el momento muestra el error en consola, debe mostralo en ventana
 			self.player.set_state(gst.STATE_NULL)
 			err, debug = message.parse_error()
 			print "Error: %s" % err, debug
-	
+			
+	# Esta funcion es recomendable que se mantenga de esta manera 
+	# He obtenido esta definicion bien estructurada de la documentacion de gstreamer
 	def on_sync_message(self, bus, message):
 		if message.structure is None:
 			return
@@ -127,12 +154,17 @@ class main:
 			imagesink.set_property("force-aspect-ratio", True)
 			gtk.gdk.threads_enter()
 			imagesink.set_xwindow_id(self.movie_window.window.xid)
-			gtk.gdk.threads_leave()	
-		
+			gtk.gdk.threads_leave()
+				
+	# Funcion que se registra al cambio en la medialist por ende en el gtk.TreeWiev
 	def on_tree_selection_changed(self,selection):
+		# Obtiene un Iterador
 		model, treeiter = selection.get_selected()
+		# Si treeiter es igual a None no hay elementos en la lista
 		if treeiter != None:
-			palabra = ""
+		# Todo esto que ven aqui es pura carpinteria, tuve que tratar la cadena 
+		# obtenida del mendialist porque venia con espacios raros y tuve que eliminarlos
+			palabra = "" 
 			contador = 0
 			for letra in model[treeiter][1]:
 				if letra != "\n":
@@ -171,10 +203,12 @@ class main:
 						nueva = nueva+letra
 						contador = contador+1			 
 			palabra = reverse(nueva)
+			# Regresa la nueva palabra sin espacios raros, ahora la ruta ya es 100 por ciento valida
 			return palabra 	
-				
+	# Muestra el mensaje de about 
 	def about(self,widget):
 		self.help.show()
+	# Cierra el mensjae de about
 	def close(self,widget):
 		self.help.hide()
 	# Funcion que cierra la ventana
@@ -186,18 +220,25 @@ class main:
 		self.stop(widget)
 		self.rep = 1
 		select = self.tree.get_selection()
+		# Llama a la funcion que trata la ruta
 		filepath = self.on_tree_selection_changed(select)
+		# Envia la ruta al objeto de gstreamer 
 		self.player.set_property("uri", "file://"+filepath)
+		# Reproduce
 		self.player.set_state(gst.STATE_PLAYING)
+		
 	def pause(self,widget):
 		# funcion de pausa
 		pass
+		
 	def prev(self,widget):
 		# funcion de anterior
 		pass
+		
 	def next(self,widget):
 		# funcion de siguiente
 		pass
+		
 	def stop(self,widget):
 		# funcion de alto
 		if self.rep == 1:
