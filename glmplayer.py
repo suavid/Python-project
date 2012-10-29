d75aebda5bbc1abde5ea72f6b8afbc0ebe530358
William David Parras <william.parras.mendez@gmail.com>
2012-10-29 09:40:53 -0600

    Eliminando la integracion con twitter

--- glmplayer.py
+++ glmplayer.py
@@ -34,23 +34,24 @@ auth = tweepy.OAuthHandler('V6yc6n5Mp7L8SATCxzrbHg','SOvHZc2w0v8XCZrBbMriat3QJfu
 class main:
 
 ##################################################################################
-# CONSTRUCTOR
+#                                                                                #
+#                                    CONSTRUCTOR                                 #
+#                                                                                #
+##################################################################################	
 	def __init__(self):
 
 		##########################################################################
    		# VENTANA PRINCIPAL
 		builder = gtk.Builder()
 		builder.add_from_file(path.PATH) 
-	 	###########################################################################
-
-	 	###########################################################################
+	 	##########################################################################
+	 	##########################################################################
 	 	# VENTANAS SECUNDARIAS	
 		self.agregar_ventana = builder.get_object("Add")
 		self.help = builder.get_object("About")
 		self.window_edicion = builder.get_object("edit")
-		self.window_nuevo_tweet = builder.get_object("window_tweet")
-		###########################################################################
-		############################################################################
+		##########################################################################
+		##########################################################################
 		# OBJETOS ACTIVOS EN LA VENTANA
 		self.t_bt_home = builder.get_object("Home")
 		self.tree = builder.get_object("arbol_pistas")
@@ -104,9 +105,8 @@ class main:
 		self.Hom = builder.get_object("Home_pane")
 		self.toogle_Follows_btn = builder.get_object("Follows")
 		self.follow_pane = builder.get_object("follow_pane")
-		############################################################################
-
-		############################################################################
+		###########################################################################
+		###########################################################################
 		# OTRAS ACCIONES
 		self.info.set_text("No se ha reproducido nada aun")
 		self.filtro = gtk.FileFilter()
@@ -125,22 +125,18 @@ class main:
 		self.clave2_2.set_invisible_char ('*') 
 		# MARCA EL PULSO DE LA BARRA DE ESTADO
 		self.plog.set_pulse_step(0.01)
-		self.t_bt_home.set_active(True)
 		self.volumen.set_value(20)
 		proc = subprocess.Popen('/usr/bin/amixer sset Master ' + str(20) + '%', shell=True, stdout=subprocess.PIPE)
 		proc.wait()
 		self.time_song = 0
-		#############################################################################
-
-
-		#############################################################################
+		############################################################################
+		############################################################################
 		# VARIABLES DE MAPEO DE REPRODUCCION
 		self.MAPA = [] 
 		self.current = 0
 		self.controler = 0
 		self.max = 0
 		#############################################################################
-
 		#############################################################################
     	# EVENTOS
 		dict = {"on_agregar_activate": self.abrir_archivos,
@@ -162,26 +158,16 @@ class main:
 		"on_nuevo_clicked":self.registrar,
 		"on_tweet_clicked":self.tuitear,
 		"on_final_clicked":self.completado,
-		"on_refresh_clicked":self.refresh,
-		"on_submit_clicked":self.submit,
 		"on_AbrirB_clicked":self.abrir_archivos,
 		"on_editar_clicked":self.edicion,
 		"on_cancel_edit_clicked":self.stop_edicion,
 		"on_ok_edit_clicked":self.save,
-		"on_N_T_toggled":self.toogle_N_T,
-		"on_Recientes_toggled":self.toogle_Recientes,
-		"on_Home_toggled":self.toogle_Home,
-		"on_Follows_toggled":self.toogle_Follows,
-		"on_HideAll_clicked":self.HideAll,
-		"on_ShowAll_clicked":self.ShowAll,
 		"on_back_clicked":self.back,
-		"on_show_tw_clicked":self.window_nuevo,
-		"on_tweet_cancelar_clicked":self.close_tweet
+		"on_close_session_clicked":self.close_session
 		}
 		#CONEXION CON EVENTOS
 		builder.connect_signals(dict)
 		#############################################################################
-
 		#############################################################################
 		# CONTROLADOR DE REPRODUCCION
 		self.player = gst.element_factory_make("playbin2", "player")
@@ -195,8 +181,11 @@ class main:
 #####################################################################################
 
 #####################################################################################
-# FUNCIONES DE TWITTER
-# FUNCION DE LANZAMIENTO 
+#                                                                                   #
+#                                 FUNCIONES DE TWITTER                              #
+#                                                                                   #
+#####################################################################################
+# FUNCION DE LANZAMIENTO DE HILO DE INICIO DE SESION 
 	def login(self,widget):
 		Log = LOG(self.login_state,self.plog,self.info)
 		Log.start()
@@ -217,50 +206,12 @@ class main:
 				auth.set_access_token(dom.getElementsByTagName("KEY")[found].firstChild.data,dom.getElementsByTagName("SECRET")[found].firstChild.data)
 				global API 
 				API = tweepy.API(auth)
-				self.recent_tweets()
-				# INICIO DE HILOS 
-				US = DEF(self.US)
-				US.start()
-				HM = DEF(self.Home)
-				HM.start()
-				#self.user.set_text( dom.getElementsByTagName("id")[found].firstChild.data )
 				self.step1.hide()
 				self.logged.show()
 			else:
 				self.status.set_text('Usuario o clave incorrecta')
  		except:
 			pass
-
-# TWEETS MAS RECIENTES PUBLICADOS POR EL USUARIO AUTENTICADO
-	def recent_tweets(self):
-		arrayObject = API.user_timeline()
-		self.tweet1.set_text(self.parrafo(arrayObject[0].text))
-		self.tweet2.set_text(self.parrafo(arrayObject[1].text))
-		self.tweet3.set_text(self.parrafo(arrayObject[2].text))
-		self.tweet4.set_text(self.parrafo(arrayObject[3].text))
-		self.tweet5.set_text(self.parrafo(arrayObject[4].text))
-		self.tweet6.set_text(self.parrafo(arrayObject[5].text))
-
-# CARGA EL HOME DEL USUARIO AUTENTICADO
-	def Home(self):
-		self.info.set_text('Cargando Home')
-		arrayObject = API.home_timeline()
-		self.h1.set_text(arrayObject[0].user.screen_name+'\n '+self.parrafo(arrayObject[0].text))
-		self.h2.set_text(arrayObject[1].user.screen_name+'\n '+self.parrafo(arrayObject[1].text))
-		self.h3.set_text(arrayObject[2].user.screen_name+'\n '+self.parrafo(arrayObject[2].text))
-		self.h4.set_text(arrayObject[3].user.screen_name+'\n '+self.parrafo(arrayObject[3].text))
-		self.h5.set_text(arrayObject[4].user.screen_name+'\n '+self.parrafo(arrayObject[4].text))
-		self.h6.set_text(arrayObject[5].user.screen_name+'\n '+self.parrafo(arrayObject[5].text))
-		self.info.set_text('Home Cargado')
-
-# CARGA LOS SEGUIDORES MAS RECIENTES
-	def US(self):
-		arrayObject = API.followers()
-		self.f1.set_text('@'+arrayObject[0].screen_name)
-		self.f2.set_text('@'+arrayObject[1].screen_name)
-		self.f3.set_text('@'+arrayObject[2].screen_name)
-		self.info.set_text('Seguidores recientes cargados')
-
 # COMPARTE LA CANCION EN REPRODUCCION
 	def song(self):
 		try:
@@ -274,118 +225,24 @@ class main:
 					else:
 						titulo = titulo + letra
 				API.update_status("#NP #"+ titulo +" a traves de #GLMPlayer")
-			self.refresh_state()
+		except NameError:
+			self.info.set_text("Primero debe iniciar sesion")
 		except:
-			self.info.set_text('Tweet duplicado, no se envia actualizacion')
-			self.refresh_state()
-
-# NUEVO TWEET
-	def ntweet(self):
-		try:
-			API.update_status(self.tweet.get_text())
-			self.tweet.set_text("")
-			self.info.set_text("tweet enviado, puede cerrar la ventana")
-		except:
-			self.info.set_text('Ha ocurrido algun error')
-		self.refresh_state()
-
-# FORMATEA LOS TWEETS 
-	def parrafo(self,string):
-		bufferstr = '\n  '
-		token_ctn = 0
-		break_ctn = 0
-		for token in string:
-			if token_ctn < 60:
-				bufferstr = bufferstr + token
-				token_ctn = token_ctn + 1
-			else:
-				if token == ' ' and break_ctn == 0:
-					bufferstr = bufferstr + '  \n  '
-					break_ctn = 1
-					token_ctn = token_ctn + 1
-				else:
-					bufferstr = bufferstr + token
-					token_ctn = token_ctn + 1
-		bufferstr = bufferstr + '\n  '
-		return bufferstr
-
-# ACTUALIZA LOS PANELES
-	def refresh(self,widget):
-		self.refresh_state()
-
-	def refresh_state(self):
-		RC = DEF(self.recent_tweets)
-		US = DEF(self.US)
-		HM = DEF(self.Home)
-		HM.start()
-		RC.start()
-		US.start()
-
-	def window_nuevo(self,widget):
-		self.window_nuevo_tweet.show()
-
-	def close_tweet(self,widget):
-		self.window_nuevo_tweet.hide()
-		self.tweet.set_text("")
-#####################################################################################
-
-#####################################################################################
-# CONTROL DEL VOLUMEN
-	def cb_master_slider_change(self, widget,event,data=None):
-		try:
-			val = widget.get_value()
-			proc = subprocess.Popen('/usr/bin/amixer sset Master ' + str(val) + '%', shell=True, stdout=subprocess.PIPE)
-			proc.wait()
-		except:
-			pass	
-#####################################################################################
-
-#################################################################################
+			self.info.set_text("Estado duplicado o verifique su conexion a internet")
+# CIERRA SESION
+	def close_session(self,widget):
+		self.step1.show()
+		self.logged.hide()	
+		global API	
+		del API
 # VERIFICACION DE LA CUENTA DE TWITTER
 	def acceso(self,widget):
 		self.step1.hide()
 		self.step2.show()
-
+# REGRESA A VENTANA DE INICIO DE SESION
 	def back(self,widget):
 		self.step1.show()
 		self.step2.hide()
-
-###################################################################################
-
-####################################################################################
-# TUITEAR
-	def tuitear(self,widget):
-		SN = DEF(self.song)
-		SN.start()
-
-	def submit(self,widget):
-		TW = DEF(self.ntweet)
-		TW.start()
-
-####################################################################################
-
-#####################################################################################
-# REGISTRO
-	def registrar(self,widget):
-		CUsuario = self.usuario2.get_text()
-		CClave = self.clave2.get_text()
-		CClave2 = self.clave2_2.get_text()
-		dom = minidom.parse(path.OAUTH)
-		count = 0
-		for i in range(0,len(dom.getElementsByTagName("id"))):
-			if CUsuario == dom.getElementsByTagName("id")[i].firstChild.data:
-				count = count + 1
-		if count > 0: 
-			self.msj.set_text('El usuario ya existe')
-		else:
-			if ( CClave == CClave2 ) and ( len(CUsuario) ) > 0 and ( len(CClave) > 5 ):
-				webbrowser.open(auth.get_authorization_url())
-			else:
-				self.msj.set_text('* Minimo 6 caracteres para la clave \n * Rellene todos los campos \n * Verifique que las claves coincidan ')
-######################################################################################
-
-######################################################################################
-# COMPLETADO
 # AQUI SE COMPLETA EL REGISTRO DE LA APLICACION
 	def completado(self,widget):
 		try:
@@ -430,8 +287,28 @@ class main:
 			self.step1.show()
 		except:
 			self.msj.set_text('Lo sentimos, el codigo no es valido,\n intenta de nuevo')
-######################################################################################
-
+# TUITEAR
+	def tuitear(self,widget):
+		SN = DEF(self.song)
+		SN.start()
+# REGISTRO
+	def registrar(self,widget):
+		CUsuario = self.usuario2.get_text()
+		CClave = self.clave2.get_text()
+		CClave2 = self.clave2_2.get_text()
+		dom = minidom.parse(path.OAUTH)
+		count = 0
+		for i in range(0,len(dom.getElementsByTagName("id"))):
+			if CUsuario == dom.getElementsByTagName("id")[i].firstChild.data:
+				count = count + 1
+		if count > 0: 
+			self.msj.set_text('El usuario ya existe')
+		else:
+			if ( CClave == CClave2 ) and ( len(CUsuario) ) > 0 and ( len(CClave) > 5 ):
+				webbrowser.open(auth.get_authorization_url())
+			else:
+				self.msj.set_text('* Minimo 6 caracteres para la clave \n * Rellene todos los campos \n * Verifique que las claves coincidan ')
+#####################################################################################
 #####################################################################################
 # AGREGAR ARCHIVOS
 	def abrir_archivos(self,widget):
@@ -463,6 +340,7 @@ class main:
 			xmldocument = open(path.TRACK,"w")
 			dom.writexml(xmldocument)
 			xmldocument.close()
+#  BORRA UN ELEMENTO DE LA LISTA
 	def delete(self,widget):
 		select = self.tree.get_selection()
 		filepath = path.on_tree_selection_changed(select)
@@ -486,13 +364,13 @@ class main:
 		xmldocument = open(path.TRACK,"w")
 		dom.writexml(xmldocument)
 		xmldocument.close()
-
+# LIMPIA TODA LA LISTA
 	def clean(self,widget):
 		doc = open(path.TRACK,"w")
 		doc.write('<?xml version="1.0" ?><wml></wml>')
 		doc.close()
 		self.medialist.clear()
-
+# EDITA TAGS DE LA PISTA SELECCIONADA
 	def edicion(self,widget):
 		try:
 			select = self.tree.get_selection()
@@ -505,10 +383,10 @@ class main:
 			self.window_edicion.show()
 		except:
 			pass
-
+# CANCELA LA EDICION DE LA PISTA SELECCIONADA
 	def stop_edicion(self,widget):
 		self.window_edicion.hide()
-
+# GUARDA LOS CAMBIOS HECHOS A LOS TAGS
 	def save(self,widget):
 		select = self.tree.get_selection()
 		filepath = path.on_tree_selection_changed(select)
@@ -519,50 +397,15 @@ class main:
 		tag.setTitle(self.stock_titulo.get_text())
 		tag.update()
 		self.window_edicion.hide()
-
-	def toogle_N_T(self,widget):
-		if self.toogle_N_T_btn.get_active():
-			self.nuevosT.show()
-		else:
-			self.nuevosT.hide()
-
-	def toogle_Recientes(self,widget):
-		if self.toogle_Recientes_btn.get_active():
-			self.Rec.show()
-		else:
-			self.Rec.hide()
-
-	def toogle_Home(self,widget):
-		if self.toogle_Home_btn.get_active():
-			self.Hom.show()
-		else:
-			self.Hom.hide()	
-
-	def toogle_Follows(self,widget):
-		if self.toogle_Follows_btn.get_active():
-			self.follow_pane.show()
-		else:
-			self.follow_pane.hide()		
-	def HideAll(self,widget):
-		self.toogle_N_T_btn.set_active(False)
-		self.toogle_Recientes_btn.set_active(False)
-		self.toogle_Home_btn.set_active(False)
-		self.toogle_Follows_btn.set_active(False)
-		self.follow_pane.hide()	
-		self.Hom.hide()
-		self.Rec.hide()
-		self.nuevosT.hide()
-	def ShowAll(self,widget):
-		self.toogle_N_T_btn.set_active(True)
-		self.toogle_Recientes_btn.set_active(True)
-		self.toogle_Home_btn.set_active(True)
-		self.toogle_Follows_btn.set_active(True)
-		self.follow_pane.show()	
-		self.Hom.show()
-		self.Rec.show()
-		self.nuevosT.show()
+# CONTROL DEL VOLUMEN
+	def cb_master_slider_change(self, widget,event,data=None):
+		try:
+			val = widget.get_value()
+			proc = subprocess.Popen('/usr/bin/amixer sset Master ' + str(val) + '%', shell=True, stdout=subprocess.PIPE)
+			proc.wait()
+		except:
+			pass
 #####################################################################################
-
 #####################################################################################			
 # MENSAJES DE ERROR PARA CONSOLA gSTREAMER
 	def on_message(self, bus, message):
@@ -574,9 +417,6 @@ class main:
 			self.player.set_state(gst.STATE_NULL)
 			err, debug = message.parse_error()
 			print "Error: %s" % err, debug
-####################################################################################
-			
-####################################################################################
 # MANEJADORA gSTREAMER                                                             
 	def on_sync_message(self, bus, message):
 		if message.structure is None:
@@ -595,11 +435,9 @@ class main:
 	# Muestra el mensaje de about 
 	def about(self,widget):
 		self.help.show()
-
 	# Cierra el mensaje de about
 	def close(self,widget):
 		self.help.hide()
-
 	# Funcion que cierra la ventana
 	def destroy(self,widget):
 		gtk.main_quit()
@@ -712,7 +550,6 @@ class main:
 		self.info.set_text(" Se ha detenido la reproduccion")
 		self.hilo.stop()	
 ###################################################################################
-
 ###################################################################################
 # MANEJO DE HILO DE PROGRESO         
 class MiThread(threading.Thread):  
@@ -773,7 +610,6 @@ class LOG(threading.Thread):
 		Pul.start()
 		self.function()
 		Pul.stop(False)
-		self.info.set_text('Tweets recientes cargados')
 
 ##################################################################################
 
